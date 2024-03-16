e2c68e4cf0b8f856c6b83bfc6c4c5e74fcd26be9
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-03-16 14:49:11 +0100

    IT WORKS

--- APIs/src/index.js
+++ APIs/src/index.js
@@ -1,1 +1,23 @@
 import "./styles.scss";
+
+const img = document.querySelector("img");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  getImage();
+});
+
+getImage();
+
+function getImage() {
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=Mx4JFbLmysBu1d0vMQ7c3dG2akarPFO6&s=${input.value}`, //i know I shoudnt
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+}
