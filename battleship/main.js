76fa828661397a1b15409039ade5c0115a592e52
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-05-13 22:34:17 +0200

    node first excercises

--- battleship/main.js
+++ battleship/main.js
@@ -18,24 +18,64 @@ function Gameboard() {
   const boardSize = 10;
   const board = [];
   for (let i = 0; i < boardSize; i++) {
-    board.push([]);
+    board.push(new Array(boardSize).fill({ canBePlaced: true }));
   }
   function addShipToBoard(row, column, size, orientation) {
     const ship = Ship(size);
-    if (orientation === "horizontal") {
+    if (checkPlaceValidity(row, column, size, orientation) === false) {
+      return false;
+    }
+    if (orientation === "vertical") {
       for (let i = 0; i < size; i++) {
         board[row][column + i] = ship;
+        forbidPlaces(row, column + i);
       }
     } else {
       for (let i = 0; i < size; i++) {
         board[row + i][column] = ship;
+        forbidPlaces(row + 1, column);
       }
     }
   }
+  function forbidPlaces(x, y) {
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        if (checkBounds(x + i, y + j)) {
+          if (board[x + i][y + j].canBePlaced === true) {
+            board[x + i][y + j] = { canBePlaced: false };
+          }
+        }
+      }
+    }
+  }
+
+  function checkPlaceValidity(row, column, size, orientation) {
+    if (orientation === "horizontal") {
+      for (let i = 0; i < size; i++) {
+        if (!checkBounds(row, column + i)) return false;
+        if (board[row][column + i].canBePlaced === false) return false;
+      }
+    }
+    if (orientation === "vertical") {
+      for (let i = 0; i < size; i++) {
+        if (!checkBounds(row + i, column)) return false;
+        if (board[row + i][column].canBePlaced === false) return false;
+      }
+    }
+    return true;
+  }
+  function checkBounds(x, y) {
+    return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
+  }
+
   return {
     board,
     addShipToBoard,
   };
 }
-Gameboard();
+
+const gameboard = Gameboard();
+gameboard.addShipToBoard(5, 5, 3, "horizontal");
+console.log(gameboard.board);
+
 module.exports = { Ship, Gameboard };
--- battleship/main.test.js
+++ battleship/main.test.js
@@ -1,5 +1,9 @@
 const { Ship, Gameboard } = require("./main");
 
+let gameboard;
+beforeEach(() => {
+  gameboard = Gameboard();
+});
 test("Ship getting hit", () => {
   const ship = Ship(3);
   ship.hit();
@@ -14,12 +18,24 @@ test("Ship sunking", () => {
   expect(ship.isSunk()).toBe(true);
 });
 
-test("Add ship to board", () => {
-  const gameboard = Gameboard();
-  gameboard.addShipToBoard(5, 5, 3, "horizontal");
+test("Add ship horizontally", () => {
+  gameboard.addShipToBoard(3, 3, 2, "horizontal");
+  expect(gameboard.board[1][3]).toStrictEqual({ canBePlaced: true });
+  expect(gameboard.board[2][3]).toStrictEqual({ canBePlaced: false });
+  expect(gameboard.board[3][3].hits).toBe(0);
+  expect(gameboard.board[4][3].hits).toBe(0);
+  expect(gameboard.board[5][3]).toStrictEqual({ canBePlaced: false });
+  expect(gameboard.board[6][3]).toStrictEqual({ canBePlaced: true });
+});
+
+test("Add ship vertically", () => {
+  gameboard.addShipToBoard(5, 5, 3, "vertical");
   gameboard.board[5][5].hit();
+  expect(gameboard.board[5][3]).toStrictEqual({ canBePlaced: true });
+  expect(gameboard.board[5][4]).toStrictEqual({ canBePlaced: false });
   expect(gameboard.board[5][5].hits).toBe(1);
   expect(gameboard.board[5][6].hits).toBe(1);
   expect(gameboard.board[5][7].hits).toBe(1);
-  expect(gameboard.board[5][8]).toBeUndefined();
+  expect(gameboard.board[5][8]).toStrictEqual({ canBePlaced: false });
+  expect(gameboard.board[5][9]).toStrictEqual({ canBePlaced: true });
 });
